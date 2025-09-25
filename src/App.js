b6b209f5b414722a2d266a256a952e480e1ea404
Mons1214 <mons.gorsor@gmail.com>
2025-09-25 15:35:54 -0600

    A login page is created (title, inputs and buttons) without logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import FilledInput from '@mui/material/FilledInput';
+import { Visibility } from '@mui/icons-material';
+import { VisibilityOff } from '@mui/icons-material';
 
 function App() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <Box className="Card" sx={{ }}> 
+        <Box>
+          <Typography variant='h4'>Bienvenido </Typography>
+        </Box>
+          /*Usuario*/
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+          <TextField label="Usuario" variant="standard" />
+        </Box>
+
+        /*Contraseña*/
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}> 
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-password">Contraseña</InputLabel>
+          <FilledInput 
+          id="filled-adornment-password" 
+          type={showPassword ? 'text' : 'password'} 
+          endAdornment={
+          <InputAdornment position="end">
+            <IconButton onClick={handleClickShowPassword}> 
+              {showPassword ? <VisibilityOff /> :<Visibility />}
+            </IconButton>
+          </InputAdornment>
+        } 
+      />
+        </FormControl>
+        </Box>
+
+        /*Botones*/
+        <Box>
+          <Button>Entrar</Button>
+          <Button>Registro</Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
