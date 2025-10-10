60fce6f5b7dbaea39a304e207b14593d8b5d0a26
Mons1214 <mons.gorsor@gmail.com>
2025-10-09 20:32:16 -0600

    A react page was created with MUI and express

---
+++ my-app-vite/src/App.jsx
@@ -0,0 +1,124 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import FilledInput from '@mui/material/FilledInput';
+import { Visibility } from '@mui/icons-material';
+import { VisibilityOff } from '@mui/icons-material';
+
+
+export default function App() {
+
+  const [Usuario, setUsuario] = useState('');
+  const [password, setPassword] = useState('');
+  const [registros, setRegistros] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(prev => !prev);
+  const handleMouseShowPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleRegistro = () => {
+    if (!Usuario.trim() || !password.trim()) {
+      alert('Por favor completa el registro');
+      return;
+    }
+
+    setRegistros(prev => [...prev, { usuario: Usuario, password }]);
+    console.log("Usuarios registrados:", registros);
+
+    setUsuario("");
+    setPassword("");
+  };
+
+
+  useEffect(() => {
+    fetch("http://localhost:3001")
+      .then(res => res.json())
+      .then(data => console.log(data.mensaje))
+      .catch(err => console.log("Error al conectar con el servidor:", err));
+  }, []);
+
+
+
+  return (
+    <Box className="App" sx={{
+      width: '100%',
+      height: '96vh',
+      border: '3px solid',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Box className="Card" sx={{
+        border: '3px solid',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        backgroundColor: 'violet',
+        padding: 3,
+        gap: 1,
+      }}>
+        <Box>
+          <Typography variant='h4'>𝔹𝕚𝕖𝕟𝕧𝕖𝕟𝕚𝕕𝕠</Typography>
+        </Box>
+
+        <Box >
+
+          <TextField
+            size="small"
+            id="outlined-controlled"
+            label="Usuario"
+            value={Usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+            color='secondary'
+            variant='outlined'
+            sx={{ mb: 1, width: '25ch' }}
+            inputProps={{ name: 'usuario' }}
+          />
+        </Box>
+
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <FormControl size="small" sx={{ m: 1, width: '25ch' }} variant="filled">
+            <InputLabel htmlFor="filled-adornment-password">Contraseña</InputLabel>
+            <FilledInput
+              id="filled-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              inputProps={{ name: 'password' }}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseShowPassword}
+                    edge="end">
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Box>
+
+        <Box>
+          <Button type='button' onClick={handleRegistro}>Registro</Button>
+        </Box>
+
+        <Box sx={{ mt: 2 }}>
+          {registros.map((reg, index) => (
+            <Typography key={index}>{reg.usuario} - {reg.password}</Typography>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+}
